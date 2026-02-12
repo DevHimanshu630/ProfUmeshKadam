d3fc7450bfc3e33009684adb3e9809028b339e72
Himanshu Singh <hsc95067@gmail.com>
2026-02-12 20:27:06 +0530

    remove contact details and DOB

--- prof-kadam-website/src/components/About.jsx
+++ prof-kadam-website/src/components/About.jsx
@@ -56,7 +56,7 @@ const About = ({ id, setActiveSection }) => {
                 <div className="flex items-center space-x-3">
                   <div className="w-2 h-2 bg-primary-500 rounded-full"></div>
                   <span className="text-secondary-700">
-                    <strong>Date of Birth:</strong> 28 May 1976
+                    <strong>Date of Birth:</strong> May 1976
                   </span>
                 </div>
                 <div className="flex items-center space-x-3">
@@ -104,7 +104,7 @@ const About = ({ id, setActiveSection }) => {
                   <svg className="w-5 h-5 text-primary-600" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                   </svg>
-                  <span className="text-secondary-700">(0) 7838489367, (0) 9850169020</span>
+                  {/* <span className="text-secondary-700">(0) 7838489367, (0) 9850169020</span> */}
                 </div>
               </div>
             </div>
