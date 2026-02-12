393bda28e5b6b9b5dd213828e5a64fc19107aa47
Himanshu Singh <hsc95067@gmail.com>
2026-02-12 20:24:14 +0530

    count static added

--- prof-kadam-website/src/components/Footer.jsx
+++ prof-kadam-website/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { getApiUrl } from '../config/api'
 
 const Footer = () => {
-  const [visitorCount, setVisitorCount] = useState('702')
+  const [visitorCount, setVisitorCount] = useState('736')
 
   useEffect(() => {
     // Fetch visitor count
